7f1295d081b8f2e5bf78ecb090c8a881027fbfda
Farmerofb <Farmerofb@hotmail.com>
2024-08-16 11:00:15 +0100

    Add BDD TESTS

---
+++ UnitTests/_PlanetWeight.feature
@@ -0,0 +1,13 @@
+﻿Feature: Calculate weight on different planets
+
+Scenario: Calculate weight on Mercury
+  Given a user enters a weight of 100 lbs
+  And selects the planet "Mercury"
+  When the weight on Mercury is calculated
+  Then the calculated weight should be 37.8 lbs
+
+Scenario: Entering an invalid weight outside the valid range
+  Given a user enters a weight of 600 lbs
+  And selects the planet "Mars"
+  When the form is submitted
+  Then an error message "Invalid weight - Only Available from 1 to 500 lbs" should be displayed
---
+++ UnitTests/_PlanetWeightSteps.cs
@@ -0,0 +1,51 @@
+﻿using TechTalk.SpecFlow;
+using PlanetWeights;
+
+[Binding]
+public class _PlanetWeightSteps
+{
+    private PlanetWeight _planetWeight;
+    private string _errorMessage;
+
+    [Given(@"a user enters a weight of (.*) lbs")]
+    public void GivenAUserEntersAWeightOfLbs(double weight)
+    {
+        _planetWeight = new PlanetWeight { weight = weight };
+    }
+
+    [Given(@"selects the planet ""(.*)""")]
+    public void GivenSelectsThePlanet(string planetName)
+    {
+        Enum.TryParse(planetName.ToLower(), out Planets planet);
+        _planetWeight.planet = planet;
+    }
+
+    [When(@"the weight on (.*) is calculated")]
+    public void WhenTheWeightOnPlanetIsCalculated(string planetName)
+    {
+        // The weight calculation happens automatically in the getter of weightOnPlanet
+        var calculatedWeight = _planetWeight.weightOnPlanet;
+    }
+
+    [Then(@"the calculated weight should be (.*) lbs")]
+    public void ThenTheCalculatedWeightShouldBeLbs(double expectedWeight)
+    {
+        Assert.Equal(expectedWeight, _planetWeight.weightOnPlanet);
+    }
+
+    [When(@"the form is submitted")]
+    public void WhenTheFormIsSubmitted()
+    {
+        // Simulate the form submission and validation
+        if (_planetWeight.weight < PlanetWeight.weightMin || _planetWeight.weight > PlanetWeight.WeightMax)
+        {
+            _errorMessage = "Invalid weight - Only Available from 1 to 500 lbs";
+        }
+    }
+
+    [Then(@"an error message ""(.*)"" should be displayed")]
+    public void ThenAnErrorMessageShouldBeDisplayed(string expectedMessage)
+    {
+        Assert.Equal(expectedMessage, _errorMessage);
+    }
+}

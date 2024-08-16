68e3f942c190a14b41d5bc0e578bbbc2181d04b4
Farmerofb <Farmerofb@hotmail.com>
2024-08-16 18:42:27 +0100

    fix tests

--- UnitTests/TravelTimeEstimatorModelTests.cs
+++ UnitTests/TravelTimeEstimatorModelTests.cs
@@ -1,9 +1,19 @@
 ﻿using PlanetWeights.Pages;
+using System.Globalization;
 
 namespace UnitTests
 {
     public class TravelTimeEstimatorModelTests
     {
+        public TravelTimeEstimatorModelTests()
+        {
+            // Set culture to invariant to avoid formatting issues in different environments
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+
+
         [Fact]
         public void OnPost_Should_ReturnErrorMessage_WhenSelectedPlanetIsNull()
         {
@@ -90,11 +100,11 @@ namespace UnitTests
         }
 
         [Theory]
-        [InlineData("Mercury", 50000, "64,42 days (0,18 years) to reach Mercury at 50000 km/h.")]
-        [InlineData("Venus", 100000, "17,25 days (0,05 years) to reach Venus at 100000 km/h.")]
-        [InlineData("Mars", 25000, "130,50 days (0,36 years) to reach Mars at 25000 km/h.")]
-        [InlineData("Jupiter", 100000, "261,96 days (0,72 years) to reach Jupiter at 100000 km/h.")]
-        [InlineData("Saturn", 200000, "265,62 days (0,73 years) to reach Saturn at 200000 km/h.")]
+        [InlineData("Mercury", 50000, "64.42 days (0.18 years) to reach Mercury at 50000 km/h.")]
+        [InlineData("Venus", 100000, "17.25 days (0.05 years) to reach Venus at 100000 km/h.")]
+        [InlineData("Mars", 25000, "130.50 days (0.36 years) to reach Mars at 25000 km/h.")]
+        [InlineData("Jupiter", 100000, "261.96 days (0.72 years) to reach Jupiter at 100000 km/h.")]
+        [InlineData("Saturn", 200000, "265.62 days (0.73 years) to reach Saturn at 200000 km/h.")]
         public void OnPost_Should_CalculateTravelTime_ForValidInputs(string planet, double speed, string expectedTravelTime)
         {
             // Arrange
